6c9454b50fd41e30a8f0083b36e18ef67798c5f2
KagoKai_WSL <kagokai123@gmail.com>
2024-03-01 15:27:48 +0700

    Added basic URDF packages for the mecabot.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/kagokai/dev_ws/src/mecabot_body
+# Build directory: /home/kagokai/dev_ws/src/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
